21912525903931d620c4685da26842466cbf1986
rsstdd <rosstod@gmail.com>
2016-12-16 14:48:53 -0800

    .env/server

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
+const five = require('johnny-five');
+const io = require('socket.io')(app);
+
 switch (app.get('env')) {
   case 'development':
     app.use(morgan('dev'));
@@ -32,11 +35,6 @@ const path = require('path');
 
 app.use(express.static(path.join('public')));
 
-const players = require('./routes/players');
-const matches = require('./routes/matches');
-const token = require('./routes/token');
-const me = require('./routes/me');
-
 // CSRF protection
 app.use((req, res, next) => {
   if (!req.path.startsWith('/api') || /json/.test(req.get('Accept'))) {
@@ -46,14 +44,45 @@ app.use((req, res, next) => {
   res.sendStatus(406);
 });
 
-app.use('/api', players);
-app.use('/api', matches);
-app.use('/api', token);
-app.use('/api', me);
-
 app.use((_req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+// Arduino board connection
+
+board.on('ready', function() {
+  console.log('Arduino connected');
+  const rgb = new five.Led.RGB([6, 5, 3]); // pins with options obj and pins obj
+  let i = 0;
+  const cols = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8F00FF'];
+
+  // var rainbow = ['#5d8aa8', '#ffbf00',' #a4c639']
+
+  this.loop(1000, function() {
+    if (i + 1 === cols.length) {
+      i = 0;
+    }
+    rgb.color(cols[i++]);
+    rgb.intensity(1000);
+  });
+});
+
+// Socket Connection handler
+//
+// io.on('connectioon', () => {
+//   console.log(socket.id);
+//
+//   socket.on('led:on', (data) => {
+//     led.on();
+//     console.log('LED ON REVIEVED');
+//   });
+//
+//   socket.on('led:off', (data) => {
+//     console.log('LED OFF RECIEVED');
+//   });
+// });
+//
+// console.log('Waiting for connection');
 
 app.use((err, _req, res, _next) => {
   if (err.output && err.output.statusCode) {
